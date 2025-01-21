342a81c72e93bf4a09227b08ecd4ec13354c4943
woogamjaa <woogamjaa@gmail.com>
2025-01-21 16:05:03 +0900

    Spring multipartResolver를 이용하여 파일 업로드 구현

--- src/main/java/com/bs/spring/board/controller/BoardController.java
+++ src/main/java/com/bs/spring/board/controller/BoardController.java
@@ -3,10 +3,10 @@ package com.bs.spring.board.controller;
 import com.bs.spring.board.model.dto.Board;
 import com.bs.spring.board.model.service.BoardService;
 import com.bs.spring.common.PageFactory;
-import com.bs.spring.memo.model.dto.Memo;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -57,11 +63,49 @@ public class BoardController {
 
 
     @PostMapping(value = "/boardwriteend.do")
-    public String insertboardList (Board board , Model model, MultipartFile upFile) { //따로 저장하는 짓을 해야함.
+    public String insertboardList (Board board , Model model, MultipartFile upFile,
+                                   HttpSession session) { //따로 저장하는 짓을 해야함.
 
         log.debug("파일명" + upFile.getOriginalFilename());
         log.debug("파일크기" + upFile.getSize());
         log.debug("{}",board);
+
+        //파일 저장하기
+        //1. 파일을 저장할 절대 경로 필요 ->
+        String path=session.getServletContext().getRealPath("/resources/upload/board");
+        File dir=new File(path);
+        if(!dir.exists()){dir.mkdirs();} // 존재하지 않으면 폴더를 생성한다.
+        if (upFile!=null){
+
+            //2.리네임규칙을 생성
+            String oriname=upFile.getOriginalFilename();
+
+            //2-1 원본 파일 확장자 가져오기
+            String ext=oriname.substring(oriname.lastIndexOf("."));
+
+            //2-2 이름값이 중복되지 않게 랜덤값 가져오기
+            int rnd=(int) (Math.random()*1000)+1;
+
+            //2-3 파일명이 중복되지 않게 업로드 날짜를 밀리세컨초까지 가져오기
+            Date d=new Date(System.currentTimeMillis());
+
+            //2-4 날짜를 문자열로 패턴에 맞춰서 변경해주기
+            SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmssSSS");
+
+            //2-5 위 생성한 데이터들로 파일명 생성하기.
+            String rename="BSLOVE_"+sdf.format(d)+"_"+rnd+ext;
+
+            //3. 생성한 파일명으로 파일 저장하기
+            //MultipartFile 클래스가 제공하는 tranferTo메소드를 이용해서 저장
+
+            try{
+                upFile.transferTo(new File(path, rename));
+            }catch(IOException e) {
+                e.printStackTrace();
+                log.error(upFile.getOriginalFilename()+"파일 저장 실패");
+            }
+        }
+
         return "redirect:/board/boardlist.do";
     }
 }
