668fb82de6e79a7a80fbc7b8847e4bd4f206f39a
woogamjaa <woogamjaa@gmail.com>
2025-02-03 11:13:35 +0900

    security:SecurityController 생성 Mapping  -> loginsuccess 성공

---
+++ src/main/java/com/bs/spring/security/controller/SecurityController.java
@@ -0,0 +1,17 @@
+package com.bs.spring.security.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class SecurityController {
+    @RequestMapping("/loginsuccess")
+    public String loginend() {
+        log.debug("로그인 성공 !!");
+        return "redirect:/";
+    }
+}
