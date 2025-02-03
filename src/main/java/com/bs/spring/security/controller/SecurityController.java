edab9254a729560be5e291f31589f0b9485df8f0
woogamjaa <woogamjaa@gmail.com>
2025-02-03 11:17:50 +0900

    security: 로그인 실패시 로직 처리

--- src/main/java/com/bs/spring/security/controller/SecurityController.java
+++ src/main/java/com/bs/spring/security/controller/SecurityController.java
@@ -4,6 +4,7 @@ package com.bs.spring.security.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +14,14 @@ public class SecurityController {
     public String loginend() {
         log.debug("로그인 성공 !!");
         return "redirect:/";
+    }
+
+    @RequestMapping("/loginfail")
+    public String loginfail(Model model) {
+        log.debug("로그인 실패");
+        model.addAttribute("msg","로그인실패");
+        model.addAttribute("loc","/login");
+        return "common/msg";
+
     }
 }
